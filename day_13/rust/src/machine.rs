5305197d48385d9b1c0c3a9297104acc78e40134
Miguel Coelho <miname>
2024-12-15 00:34:57 +0000

    part _ 1 day _13

--- day_13/rust/src/machine.rs
+++ day_13/rust/src/machine.rs
@@ -1,25 +1,106 @@
+use std::{cmp::Ordering, ops::Sub};
 
 pub struct Machine {
     button_a: (usize, usize),
     button_b: (usize, usize),
-    prize: (usize, usize)
+    prize: (usize, usize),
 }
 
 impl Machine {
-
     pub fn new(button_a: (usize, usize), button_b: (usize, usize), prize: (usize, usize)) -> Self {
         Self {
             button_a,
             button_b,
-            prize
+            prize: (prize.0 + 10000000000000, prize.1 + 10000000000000),
         }
     }
 
-
     pub fn calculate_best_token_usage(&self) -> usize {
+        let result: Option<(usize, usize)> = match self.button_b.0.cmp(&self.button_b.1) {
+            Ordering::Greater => {
+                let mut start_with = self.prize.0 / self.button_b.0;
+                let result: Option<(usize, usize)>;
+                loop {
+                    let possible_ratio = Machine::calculate_possible_ration(
+                        &mut start_with,
+                        self.button_b.0,
+                        self.button_a.0,
+                        self.prize.0,
+                    );
 
-        unimplemented!()
+                    if let Some((b_clicks, a_clicks)) = possible_ratio {
+                        if a_clicks * self.button_a.1 + *b_clicks * self.button_b.1 == self.prize.1
+                        {
+                            result = Some((a_clicks, *b_clicks));
+                            break;
+                        }
+                    } else {
+                        result = None;
+                        break;
+                    }
+                    if start_with == 0 {
+                        result = None;
+                        break;
+                    }
+                    start_with -= 1;
+                }
+                result
+            }
+            _ => {
+                let mut start_with = self.prize.1 / self.button_b.1;
+                let result: Option<(usize, usize)>;
+                loop {
+                    let possible_ratio = Machine::calculate_possible_ration(
+                        &mut start_with,
+                        self.button_b.1,
+                        self.button_a.1,
+                        self.prize.1,
+                    );
+
+                    if let Some((b_clicks, a_clicks)) = possible_ratio {
+                        if a_clicks * self.button_a.0 + *b_clicks * self.button_b.0 == self.prize.0
+                        {
+                            result = Some((a_clicks, *b_clicks));
+                            break;
+                        }
+                    } else {
+                        result = None;
+                        break;
+                    }
+                    if start_with == 0 {
+                        result = None;
+                        break;
+                    }
+                    start_with -= 1;
+                }
+                result
+            }
+        };
+
+        if let Some((a, b)) = result {
+            return a * 3 + b;
+        }
+        return 0;
     }
 
+    pub fn calculate_possible_ration<'a>(
+        start_with: &'a mut usize,
+        a: usize,
+        b: usize,
+        prize: usize,
+    ) -> Option<(&'a mut usize, usize)> {
+        let rest = prize - (a * *start_with);
 
+        if rest % b == 0 {
+            return Some((start_with, rest / b));
+        }
+
+        if *start_with == 0 {
+            return None;
+        }
+
+        *start_with -= 1;
+
+        return Machine::calculate_possible_ration(start_with, a, b, prize);
+    }
 }
--- day_13/rust/src/main.rs
+++ day_13/rust/src/main.rs
@@ -3,19 +3,21 @@ use std::fs;
 use machine::Machine;
 use regex::Regex;
 
-
 mod machine;
 
 fn main() {
     let vec_machines = read_file();
 
-    let token_sum = vec_machines.iter().map(|f| f.calculate_best_token_usage()).sum::<usize>();
+    let token_sum = vec_machines
+        .iter()
+        .map(|f| f.calculate_best_token_usage())
+        .sum::<usize>();
 
     println!("Tokens : {}", token_sum);
 }
 
 fn read_file() -> Vec<Machine> {
-    let input =String::from_utf8(fs::read("test.txt").unwrap()).unwrap();
+    let input = String::from_utf8(fs::read("input.txt").unwrap()).unwrap();
 
     let reg = Regex::new(r".+X.(?<a_x>\d+).+Y.(?<a_y>\d+)\n.+X.(?<b_x>\d+).+Y.(?<b_y>\d+)\n.+X.(?<prize_x>\d+).+Y.(?<prize_y>\d+)").unwrap();
 
@@ -25,9 +27,18 @@ fn read_file() -> Vec<Machine> {
 
     for capture in captures {
         // let cap = capture.extract();
-        let button_a = (capture["a_x"].parse::<usize>().unwrap(), capture["a_y"].parse::<usize>().unwrap());
-        let button_b = (capture["b_x"].parse::<usize>().unwrap(), capture["b_y"].parse::<usize>().unwrap());
-        let prize = (capture["prize_x"].parse::<usize>().unwrap(), capture["prize_y"].parse::<usize>().unwrap());
+        let button_a = (
+            capture["a_x"].parse::<usize>().unwrap(),
+            capture["a_y"].parse::<usize>().unwrap(),
+        );
+        let button_b = (
+            capture["b_x"].parse::<usize>().unwrap(),
+            capture["b_y"].parse::<usize>().unwrap(),
+        );
+        let prize = (
+            capture["prize_x"].parse::<usize>().unwrap(),
+            capture["prize_y"].parse::<usize>().unwrap(),
+        );
         result.push(Machine::new(button_a, button_b, prize));
     }
 
